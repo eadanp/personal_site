da4bf81333ca82f5e8e68a9b4845d811268490cc
Eadan Plotnizky <eadanp@gmail.com>
2024-07-04 05:33:40 -0400

    feat: vercel insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
-<script lang="ts">
+<script>
   import "../app.css";
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+  injectSpeedInsights();
 </script>
 
 <Header />
