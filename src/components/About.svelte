ebddfd8220cc142520778a68da6a654646bf15f6
Eadan Plotnizky <eadanp@gmail.com>
2024-07-05 01:33:19 -0400

    chore: make about  a component

---
+++ src/components/About.svelte
@@ -0,0 +1,43 @@
+<div
+  class="mx-auto flex flex-col items-center justify-center gap-12 p-4 sm:gap-20 sm:p-16 md:gap-28 md:p-32"
+>
+  <h1 class="mx-auto poppins font-semibold text-3xl md:text-5xl">I am...</h1>
+  <div class="flex flex-col gap-12 w-full mx-auto sm:gap-16 md:gap-20">
+    <div class="flex flex-col gap-4 sm:flex-row sm:gap-8">
+      <p class="poppins text-4xl md:text-6xl text-indigo-500 font-semibold">
+        01
+      </p>
+      <div class="flex flex-col gap-4 sm:gap-8">
+        <h3 class="text-2xl md:text-5xl">a developer</h3>
+        <p>
+          Currently I work as a Backend Software Engineer at <a
+            href="https://www.outboundiq.com"
+            ><span class="text-indigo-700">outboundIQ</span></a
+          > where I help solve complex problems and build scalable solutions. I have
+          experience with Python, TypeScript, C# and C++ as well as cloud technologies
+          like AWS and GCP. I am always looking for new challenges and opportunities
+          to learn and grow.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 sm:flex-row sm:gap-8">
+      <p class="poppins text-4xl md:text-6xl text-indigo-500 font-semibold">
+        02
+      </p>
+      <div class="flex flex-col gap-4 sm:gap-8">
+        <h3 class="text-2xl md:text-5xl">a student</h3>
+        <p>
+          I am a computer science and cybersecurity student at <a
+            href="https://www.fau.edu"
+            ><span class="text-indigo-700">
+              Florida Atlantic University
+            </span></a
+          >
+          where I continue to learn and grow my skills. I am also working on a research
+          in the field of the Internet of Battlefield Things (IoBT) where I contribute
+          to academic papers that are under review or upcoming publications.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
